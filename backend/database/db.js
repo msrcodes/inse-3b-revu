459c9217ffe2dff416f8eaaeabad0afba3d4fca8
Digital <josephpro42@live.co.uk>
2020-02-07 12:32:55 +0000

    Bump last

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,6 +4,6 @@ const { Pool } = require('pg'),
 const pool = new Pool(config.database);
 pool.connect()
 	.then(() => console.log('[DATABASE] Successfully connected to the database.'))
-	.catch(reason => console.log(`[DATABASE] Unable to connect to the database. Reason: ${reason}`));
+	.catch(error => console.log(`[DATABASE] Unable to connect to the database. ${error}`));
 
 module.exports = pool;
