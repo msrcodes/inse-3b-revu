05a9d690acf6a644a862bfb9ef2fa5f48318a54d
Digital <josephpro42@live.co.uk>
2020-02-13 21:36:55 +0000

    Cleanup account manager, switch promise chains to async/await
    async/await is much more readable when you have lots of promises.

--- backend/manager/account.js
+++ backend/manager/account.js
@@ -1,124 +1,101 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const db = require('../database/db.js');
-const bcrypt = require('bcrypt');
-const uuidGen = require('uuid');
+const express = require('express'),
+	cookieParser = require('cookie-parser'),
+	bcrypt = require('bcrypt'),
+	uuidGen = require('uuid'),
+	db = require('../database/db.js'),
+	mailer = require('../mailer.js');
 
-
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
 const router = new express.Router();
-
 router.use(cookieParser());
 
-const mailer = require('../mailer.js');
-
-
 function getUser(req) {
-	return new Promise((resolve, reject)=> {
+	return new Promise((resolve, reject) => {
 		const token = req.cookies.session;
 
-		db.query('SELECT * FROM users inner join user_session us on users.email = us.email where token = $1', [token]).then(dbRes => {
+		db.query('SELECT * FROM users INNER JOIN user_session us ON users.email = us.email WHERE token = $1', [token]).then(dbRes => {
 			if (dbRes.rowCount) {
 				resolve(dbRes.rows[0]);
-			} else {
-				reject();
 			}
+
+			reject();
 		})
 	})
-
 }
 
+router.post('/register', async (req, res) => {
+	const { email, password, repeatPassword } = req.body;
 
-// Routes
-router.get('/', (req, res) => {
-	res.send('account test');
-});
+	if (!email || !password || !repeatPassword)
+		return res.status(400).send({error: "Missing parameters (email, password, repeatPassword)."});
 
+	if (repeatPassword !== password) 
+		return res.status(400).send({error: "Password does not match with repeat password."});
 
-router.post('/register', (req, res) => {
-	const {email, password, repeatPassword} = req.body;
+	if (!EMAIL_REGEX.test(email))
+		return res.status(400).send({error: "Invalid email address."});
 
-	if (!email || !password || !repeatPassword) {
-		res.status(400).send({error: "Requires email, password and repeatPassword"});
-		return;
-	}
+	if (!email.endsWith('.ac.uk'))
+		return res.status(400).send({error: "Email must be a valid ac.uk address."});
 
-	if (repeatPassword !== password) {
-		res.status(400).send({error: "Repeat password must match password"});
-		return;
-	}
+	try {
+		const emailDbRes = await db.query('SELECT email FROM users WHERE email = $1', [email]);
+		if (emailDbRes.rowCount > 0)
+			return res.status(400).send({error: "Email already in use."});
 
-	if (!email.endsWith('.ac.uk')) {
-		res.status(400).send({error: "Email must end in .ac.uk"});
-		return;
-	}
+		const passwordHash = await bcrypt.hash(password, 10);
+		const uuid = uuidGen();
 
-	db.query('SELECT email FROM users WHERE email = $1', [email]).then(dbRes => {
-		if (dbRes.rowCount) {
-			res.status(400).send({"error": "Email already in use"});
-		} else {
-			bcrypt.hash(password, 10).then(password_hash => {
+		const insertDbRes = await db.query('INSERT INTO users (email, password_hash, verification_token) VALUES ($1, $2, $3)', [email, passwordHash, uuid]);
+		if (insertDbRes.rowCount) {
+			mailer.sendMail(email, "Verify email for REVU",
+				`<a href='https://revu.aitken.io/api/v1/account/verify/${uuid}'>Verify your account</a>`,
+				`https://revu.aitken.io/api/v1/account/verify/${uuid}`);
 
-				const uuid = uuidGen();
-
-				db.query('insert into users (email, password_hash, verification_token) values ($1, $2, $3)', [email, password_hash, uuid]).then(insertRes => {
-					if (insertRes.rowCount) {
-						mailer.sendMail(email, "Verify email for REVU",
-							`<a href='https://revu.aitken.io/api/v1/account/verify/${uuid}'>Verify your account</a>`,
-							`https://revu.aitken.io/api/v1/account/verify/${uuid}`);
-						res.send();
-					} else {
-						res.status(500).send();
-					}
-				}).catch(err => {
-					res.status(500).send();
-				})
-
-			}).catch(reason => {
-				res.status(500).send();
-			})
+			return res.status(200).send();
 		}
-	}).catch(() => {
-		res.status(500).send();
-	})
+	} catch (error) {
+		console.error(error);
+		return res.status(500).send();
+	}
+
+	return res.status(404).send();
 });
 
+router.get('/verify/:token', async (req, res) => {
+	const { token } = req.params;
 
-router.get('/verify/:verifyToken', (req, res) => {
-	const verifyToken = req.params.verifyToken;
-
-	db.query('select email from users where verification_token = $1', [verifyToken]).then(selectRes => {
-		if (selectRes.rowCount) {
-			db.query('update users set verification_token = NULL, verified = TRUE where email = $1', [selectRes.rows[0].email]).then(() => {
-				res.status(301).redirect('https://revu.aitken.io')
-			}).catch(err => {
-				res.status(500).send();
-			})
-		} else {
-			res.status(404).send({error: "Token not found"})
+	try {
+		let emailDbRes = await db.query('SELECT email FROM users WHERE verification_token = $1', [token]);
+		if (emailRes.rowCount > 0) {
+			await db.query('UPDATE users SET verification_token = NULL, verified = TRUE WHERE email = $1', [emailDbRes.rows[0].email]);
+			return res.status(301).redirect('/');
 		}
-	}).catch(err => {
-		res.status(500).send();
-	});
 
+		return res.status(404).send({error: "Token not found"});
+	} catch (error) {
+		console.error(error);
+		return res.status(500).send();
+	}
 });
-
 
 router.post('/login', (req, res) => {
-	const {email, password} = req.body;
+	const { email, password } = req.body;
 
 	getUser(req).then(() => {
 		res.status(400).send('already logged in');
 	}).catch(() => {
-		db.query('SELECT password_hash, verification_token from users where email = $1', [email]).then(dbRes => {
+		db.query('SELECT password_hash, verification_token FROM users WHERE email = $1', [email]).then(dbRes => {
 			if (!dbRes.rowCount) {
-				res.status(400).send('Account does not exist.');
+				res.status(400).send({error: 'No account exists with that email.'});
 				return;
 			}
 			const account = dbRes.rows[0];
 
-			if (account.verification_token) { // Must be verified to login
-				res.status(401).send('Account has not been verified.');
+			// Must be verified to login
+			if (account.verification_token) {
+				res.status(401).send({error: 'Account has not been verified.'});
 				return;
 			}
 
@@ -127,13 +104,13 @@ router.post('/login', (req, res) => {
 					let uuid = uuidGen();
 					res.cookie('session', uuid, {});
 
-					db.query('insert into user_session values ($1, $2, now())', [email, uuid]).then(dbRes => {
+					db.query('INSERT INTO user_session VALUES ($1, $2, now())', [email, uuid]).then(dbRes => {
 						res.send();
 					}).catch(() => {
 						res.status(500).send();
 					});
 				} else {
-					res.status(401).send('Incorrect password.');
+					res.status(401).send({error: 'Incorrect email or password.'});
 					return;
 				}
 			})
@@ -142,17 +119,11 @@ router.post('/login', (req, res) => {
 	});
 });
 
-
 router.get('/loggedIn', (req, res) => {
-	getUser(req).then(() => {
-		res.status(200).send({loggedIn: true})
-	}).catch(() => {
-		res.status(200).send({loggedIn: false})
-	})
+	getUser(req)
+		.then(() => res.status(200).send({loggedIn: true}))
+		.catch(() => res.status(200).send({loggedIn: false}));
 });
-
-
-
 
 module.exports = {
 	router
