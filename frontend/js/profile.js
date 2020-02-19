8b50050eb98697a669b098686c78cc563d9f0176
mikaelsrozee <mikaelsrozee@gmail.com>
2020-02-19 14:47:26 +0000

    redirect user if they try to access the profile page whilst not logged in

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -2,6 +2,22 @@
 
 const handles = {};
 
+/**
+ * Used to check if the user is currently logged in, and redirects the user to the homepage if they're not
+ * @returns {Promise<void>}
+ */
+async function checkAndRedirect() {
+	const response = await fetch('api/v1/account/loggedIn');
+
+	if (response.ok) {
+		const obj = await response.json();
+		if (!obj.loggedIn)
+			window.location = window.location.toString().substring(0, window.location.toString().length - "profile.html".length) + "app.html"; // TODO: this, better.
+	} else {
+		console.log('unable to retrieve login status', response); // TODO: proper error handling
+	}
+}
+
 async function logout() {
 	// Check that the user has a session cookie
 	if (document.cookie.split(';').filter((item) => item.trim().startsWith('session=')).length) {
@@ -22,8 +38,10 @@ function addEventListeners() {
 }
 
 function onLoad() {
-	getHandles();
-	addEventListeners();
+	checkAndRedirect().then(() => {
+		getHandles();
+		addEventListeners();
+	});
 }
 
 window.addEventListener('load', onLoad);
