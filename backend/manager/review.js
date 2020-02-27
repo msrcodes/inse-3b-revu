0cd4c1a62f441e883262d5bc417c8a7e5e66319b
Digital <josephpro42@live.co.uk>
2020-02-27 14:39:16 +0000

    Start review create route

--- backend/manager/review.js
+++ backend/manager/review.js
@@ -5,12 +5,12 @@ const express = require('express'),
 const router = new express.Router();
 
 //--- Functions
-const createReview = (username) => {
-
+const createReview = (universityName, staffRating, staffReview, facilityRating, facilityReview, universityRating, universityReview, accommodationRating, accommodationReview) => {
+  return false;
 };
 
-const validateReview = (username) => {
-
+const validateReview = (universityName, staffRating, staffReview, facilityRating, facilityReview, universityRating, universityReview, accommodationRating, accommodationReview) => {
+  return false;
 };
 
 const updateReview = (username) => {
@@ -31,6 +31,18 @@ router.get('/university/:ID', async (req, res) => {
   req.params.ID = Number(req.params.ID);
   const reviews = await getUniversityReviews(req.params.ID);
   return res.status(HTTP.OK).send(reviews.rows);
+});
+
+router.post('/create', async (req, res) => {
+  const { universityName, staffRating, staffReview, facilityRating, facilityReview, universityRating, universityReview, accommodationRating, accommodationReview } = req.body;
+
+  //--- Validate & create
+  if (validateReview(universityName, staffRating, staffReview, facilityRating, facilityReview, universityRating, universityReview, accommodationRating, accommodationReview)) {
+    await createReview(universityName, staffRating, staffReview, facilityRating, facilityReview, universityRating, universityReview, accommodationRating, accommodationReview);
+    return res.status(HTTP.OK).send();
+  }
+
+  return res.status(HTTP.BAD_REQUEST).send();
 });
 
 module.exports = {
