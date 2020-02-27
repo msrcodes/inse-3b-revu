0619d3f23d7921aa4378043f79b097b6b5f66fea
ashleycma <ashley@aitken.io>
2020-02-27 22:46:04 +0000

    Add uni manager

--- backend/api.js
+++ backend/api.js
@@ -7,9 +7,11 @@ apiRouter.use(express.json());
 
 // Manager initialization
 const accountManager = require('./manager/account');
+const uniManager = require('./manager/uni');
 const reviewManager = require('./manager/review');
 
 apiRouter.use('/account', accountManager.router);
+apiRouter.use('/uni', uniManager.router);
 apiRouter.use('/review', reviewManager.router);
 
 module.exports = apiRouter;
---
+++ backend/manager/uni.js
@@ -0,0 +1,42 @@
+const db = require('../database/db.js');
+
+
+
+const express = require('express');
+const router = new express.Router();
+
+
+router.get('/uniNames', (req, res) => {
+	db.query('SELECT uni_name from university').then(dbRes => {
+		res.send(dbRes.rows)
+	}).catch(() => {
+		res.status(500).send();
+	})
+});
+
+
+// End url is /uni/:uniId
+router.get('/:uniId', (req, res) => {
+	const uniId = req.params.uniId;
+
+	console.log(uniId);
+
+	db.query('SELECT uni_name, uni_url, uni_description from university where uni_id = $1',
+		[uniId]).then(dbRes => {
+			if (dbRes.rowCount) {
+				res.send(dbRes.rows[0]);
+			} else {
+				res.status(404).send();
+			}
+	}).catch(() => {
+		res.status(500).send();
+	})
+});
+
+
+
+
+
+module.exports = {
+	router,
+};
