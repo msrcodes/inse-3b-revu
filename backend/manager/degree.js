673299fc3d6ab85f3f31901e825867c788e86dc6
HarryJLFoster <33813683+HarryJLFoster@users.noreply.github.com>
2020-03-04 18:22:06 +0000

    Add jsdoc to degree manager

--- backend/manager/degree.js
+++ backend/manager/degree.js
@@ -1,9 +1,21 @@
+/**
+ * @namespace manager.degree
+ * @description Manager that holds api endpoints and functions working with degrees
+ */
+
 const express = require('express'),
 	HTTP = require('http-status-codes'),
 	db = require('../database/db.js');
 
 const router = new express.Router();
 
+
+/**
+ * @memberOf manager.degree
+ * @function /degree/degreeNames
+ * @param req {Object} express request object
+ * @param res {Object} express response object
+ */
 router.get('/degreeNames', (req, res) => {
 	db.query('SELECT degree_name from degree').then(dbRes => {
 		res.send(dbRes.rows)
@@ -12,6 +24,14 @@ router.get('/degreeNames', (req, res) => {
 	})
 });
 
+
+/**
+ * @memberOf manager.degree
+ * @function /degree/:degreeId
+ * @param req {Object} express request object
+ * @param req.params.degreeId {String} The id of the degree to return
+ * @param res {Object} express response object
+ */
 router.get('/:degreeId', (req, res) => {
 	const degreeId = req.params.degreeId;
 
