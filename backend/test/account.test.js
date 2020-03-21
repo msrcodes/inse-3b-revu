95f1fdc6e693b7a56469165ae05311d850d115c1
ashleycma <ashley@aitken.io>
2020-03-21 22:10:27 +0000

    Add testing for /account/loggedIn

--- backend/test/account.test.js
+++ backend/test/account.test.js
@@ -69,11 +69,36 @@ describe('validate /loggedIn', () => {
 		}).then(res => {
 			if (res.ok) {
 				return res.json().then(json => {
-					expect(json).toBe
+					expect(json.loggedIn).toBe(false)
 				})
 			} else {
 				expect(res.ok).toBe(true);
 			}
+		})
+	});
+
+
+	test('Logged in', () => {
+		return fetch('http://localhost:3005/api/v1/account/login', {
+			method: 'POST',
+			body: JSON.stringify({email: 'testuser@myport.ac.uk', password: 'password'}),
+			headers: { 'Content-Type': 'application/json' },
+		}).then(res => {
+
+			return fetch('http://localhost:3005/api/v1/account/loggedIn', {
+				method: 'GET',
+				headers: {
+					cookie: res.headers.get('set-cookie')
+				}
+			}).then(res => {
+				if (res.ok) {
+					return res.json().then(json => {
+						expect(json.loggedIn).toBe(true)
+					})
+				} else {
+					expect(res.ok).toBe(true);
+				}
+			})
 		})
 	})
 })
